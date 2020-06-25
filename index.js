e16b33aa303c783a36384ba8b08ab7ff05a241d8
Federico.Ayub <federicoayub1@gmail.com>
2020-06-25 01:20:09 -0300

    agrega index.html

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const port = process.env.PORT || 6000;
 const app = express();
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.send('novitas');
 });
